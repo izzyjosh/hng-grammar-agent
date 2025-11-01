3c84750f048479469a02f7b6e2adfd72cfa790da
izzyjosh <calebayuba222@gmail.com>
2025-11-01 10:46:59 +0100

    setup

--- agent/agent.py
+++ agent/agent.py
@@ -15,6 +15,8 @@ from schemas import GrammarResponse
 load_dotenv()
 
 
+GOOGLE_API_KEY = os.getenv("GEMINI_API_KEY")
+print(GOOGLE_API_KEY)
 class GrammarAgent:
     SYSTEM_INSTRUCTIONS = (
             "You are a specialized assistant that helps users correct grammar, spelling, "
@@ -24,9 +26,8 @@ class GrammarAgent:
             )
 
     def __init__(self):
-        api_key = os.environ.get("GEMINI_API_KEY")
-        print(api_key)
-        provider = GoogleProvider(api_key=os.environ.get("GEMINI_API_KEY"))
+    
+        provider = GoogleProvider(api_key=GOOGLE_API_KEY)
         if not provider:
             raise HTTPException(status_code=404, detail="no api key")
         model = GoogleModel("gemini-2.0-flash", provider=provider)
